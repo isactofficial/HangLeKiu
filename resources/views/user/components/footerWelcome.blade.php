2db7254f4f9b3dc87e0b36eb975bce3b76f2fab3
MFarhan21004 <mfarhan2104@upi.edu>
2026-04-20 18:52:36 +0700

    merge push

--- resources/views/user/components/footerWelcome.blade.php
+++ resources/views/user/components/footerWelcome.blade.php
@@ -26,11 +26,10 @@
                                 d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z" />
                         </svg>
                     </a>
-                    <a href="https://www.youtube.com/"
-                        class="w-10 h-10 bg-[#582C0C] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
+                    <a href="https://maps.app.goo.gl/audBsxjNhuRwebng8"
+                        class="w-10 h-10 bg-[#582C0C] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity" target="_blank" rel="noopener">
                         <svg class="w-5 h-5 text-[#E5D6C5]" fill="currentColor" viewBox="0 0 24 24">
-                            <path
-                                d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 6.16 11.37 6.43 11.62.36.33.88.33 1.24 0C12.84 20.37 19 14.25 19 9c0-3.87-3.13-7-7-7zm0 9.5A2.5 2.5 0 1 1 12 6a2.5 2.5 0 0 1 0 5z" />
                         </svg>
                     </a>
                     <a href="https://www.linkedin.com/"
@@ -80,9 +79,9 @@
                 </a>
 
                 {{-- Address --}}
-                <p class="text-[16px] font-normal text-[#582C0C] leading-relaxed max-w-[280px] md:max-w-none text-center md:text-left">
+                <a href="{{ route('klinik') }}" class="text-[16px] font-normal text-[#582C0C] leading-relaxed max-w-[280px] md:max-w-none text-center md:text-left hover:text-[#A0703E] transition-colors block" style="text-decoration:underline; cursor:pointer;">
                     {{ $clinicProfile->address ?? 'Jl. Hang Lekiu V No.8, Gunung, Kec. Kby. Baru, Kota Jakarta Selatan, 12120' }}
-                </p>
+                </a>
             </div>
 
         </div>
